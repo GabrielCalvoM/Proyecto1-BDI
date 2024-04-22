913aec0f37af1a715c3a1d060c3d11fc2f47995e
unknown <gabriel.g.calvo.m@gmail.com>
2024-04-22 14:00:06 -0600

    UI

--- ProyectoBD/src/Control/sysConnection.java
+++ ProyectoBD/src/Control/sysConnection.java
@@ -3,8 +3,9 @@ package Control;
 import java.sql.*;
 
 public class sysConnection {
-        public static Connection getConnection() {
-         String DBname = "jdbc:oracle:thin:@//localhost:1521/LoscraDB"; // Carlos Connection
+    public static Connection getConnection() {
+        String DBname = "jdbc:oracle:thin:@//localhost:1521/BasesI"; // Gabriel Connection
+        //String DBname = "jdbc:oracle:thin:@//localhost:1521/LoscraDB"; // Carlos Connection
         //String DBname = "jdbc:oracle:thin:@localhost:1521:XE"; // Adrian Connection
         String user = "proy1";
         String password = "proy1";
